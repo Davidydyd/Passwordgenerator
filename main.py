e0086ba9356bc6f4ab8f44e4a1a36acb4d01a7f6
DavidSchick <20250705-DavidSchick@users.noreply.replit.com>
2023-05-24 23:37:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import * 
+import random
+
+
+commonWords = ['cat','dog','jump','train','soccer','Bayern','show','movie','series','phone']
+specialChars =['!','&','#','%','&']
+
+
+def create_password():
+  password = random.choice(commonWords) + random.choice(specialChars) + random.choice(commonWords) + str(random.randint(0,100)) + random.choice(specialChars)
+  print(password)
+
+window = Tk()
+window.title('Password')
+
+button1 = Button(window, text = "Click me", command = create_password)
+button1.pack()
+
+#img = PhotoImage(file = "lock.png")
+#lock =Canvas.create_image(100,100, image = img)
+
+window.mainloop()
